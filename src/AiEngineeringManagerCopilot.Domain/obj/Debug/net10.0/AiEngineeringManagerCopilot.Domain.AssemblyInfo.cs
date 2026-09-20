6c96cf92e43edf7cb9e6a7c2bc8c913df1b3faf5
charlesikeng-tech <charlesikeng@gmail.com>
2026-09-20 11:26:16 +0200

    update gitignore

--- src/AiEngineeringManagerCopilot.Domain/obj/Debug/net10.0/AiEngineeringManagerCopilot.Domain.AssemblyInfo.cs
+++ src/AiEngineeringManagerCopilot.Domain/obj/Debug/net10.0/AiEngineeringManagerCopilot.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiEngineeringManagerCopilot.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4c2b908cfa8393b7bc287734103ba9f917033e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3e34ddd116c9098f2ee5497f6f4af0316ef21f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiEngineeringManagerCopilot.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiEngineeringManagerCopilot.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
